1d66c109d387268018e6d4e1cfca2400f8a2193d
romi <romi.varnier@gmail.com>
2023-03-05 18:03:54 +0100

    Home modification

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,21 +7,27 @@ import bgImage3 from '../../assets/new-beer.png'
 
 const Home = () => {
   return (
-    <div>
+    <div className='Home'>
       <div className='service-box'>
-        <img src={bgImage} alt="beers" width={450}/>
-        <Link to="/beers"><h1>All Beers</h1></Link>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime porro architecto accusamus voluptatem, fuga nostrum sapiente repellat! Fugiat quis dolor eligendi itaque laboriosam, deleniti sint sequi quibusdam beatae praesentium eaque!</p>
+        <img src={bgImage} alt="beers" />
+        <div  className='content'>
+          <Link to="/beers"><h1>All Beers</h1></Link>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime porro architecto accusamus voluptatem, fuga nostrum sapiente repellat! Fugiat quis dolor eligendi itaque laboriosam, deleniti sint sequi quibusdam beatae praesentium eaque!</p>
+        </div>
       </div>
       <div className='service-box'>
-        <img src={bgImage2} alt="beers" width={450}/>
-        <Link to="/random-beer"><h1>Random Beer</h1></Link>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, aliquam voluptatum? Porro nihil veritatis optio, magnam dolorum animi cupiditate error temporibus dolor sed! Quo, dicta sunt expedita ut quibusdam pariatur.</p>
+        <img src={bgImage2} alt="beers" />
+        <div className='content'>
+          <Link to="/random-beer"><h1>Random Beer</h1></Link>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, aliquam voluptatum? Porro nihil veritatis optio, magnam dolorum animi cupiditate error temporibus dolor sed! Quo, dicta sunt expedita ut quibusdam pariatur.</p>
+        </div>
       </div>
       <div className='service-box'>
-        <img src={bgImage3} alt="beers" width={450}/>
-        <Link to="/new-beer"><h1>New Beer</h1></Link>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia mollitia repellat reiciendis amet nesciunt, itaque veritatis optio, magni nam assumenda repudiandae quia harum fugiat qui modi. Natus ipsa vel consequatur?</p>
+        <img src={bgImage3} alt="beers" />
+        <div className='content'>
+          <Link to="/new-beer"><h1>New Beer</h1></Link>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia mollitia repellat reiciendis amet nesciunt, itaque veritatis optio, magni nam assumenda repudiandae quia harum fugiat qui modi. Natus ipsa vel consequatur?</p>
+        </div>
       </div>
     </div>
   )
